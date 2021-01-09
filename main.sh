af50510bbde5492ddda6d55958990b9e6fc3ebee
SheNNawY <devshennawy@gmail.com>
2021-01-09 03:05:06 +0200

    firt main function

--- main.sh
+++ main.sh
@@ -1,7 +1,31 @@
 #!/usr/bin/bash
 
+clear
+projDir="$HOME/SSDBMS"
+mkdir -p $projDir
+
 echo "#########################################################"
 echo "#                                                       #"
 echo "# ----------------- Welcome to SSDBMS ----------------- #"
 echo "#                                                       #"
 echo "#########################################################"
+
+mainMenu() {
+	echo "1) Select database"
+	echo "2) Create database"
+	echo "3) Rename database"
+	echo "4) Drop database"
+	echo "5) Exit"
+
+	read 
+	case $REPLY in 
+		1 ) echo "Select database logic";;
+		2 ) echo "Create database logic";;
+		3 ) echo "Rename database logic";;
+		4 ) echo "Drop database logic";;
+		5 ) exit;;
+		* ) echo -e "Wrong choice\n"; mainMenu ;;
+	esac
+}
+
+mainMenu
