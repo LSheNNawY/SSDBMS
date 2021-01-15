0af550762691a2fead4bdb0e5e6d6d081a496f4e
SheNNawY <devshennawy@gmail.com>
2021-01-15 04:41:51 +0200

    Edit select from table

--- .selectFromTable.sh
+++ .selectFromTable.sh
@@ -1,48 +1,59 @@
 #!/usr/bin/zsh
 
-selectingFromTable () {
+selectingFromTable() {
     read -p "Enter table name: " tableName
 
     if [[ $tableName == "" || ! -f "$projDir/$DBName/$tableName" ]]; then
         echo -e "\n${yellow}Table not found!${reset}\n"
     else
-       # check if there is no data
-       if [[ `cat "$projDir/$DBName/$tableName" | wc -l` == 0 ]]; then
-           echo -e "\n${yellow}No data found${reset}\n"
-       else
-           # get table columns name
-           colNum=`head -n 1 "$projDir/$DBName/.$tableName" | cut -d',' -f2`
-           colNames=`tail -n+2 "$projDir/$DBName/.$tableName" | cut -d',' -f1`
+        # check if there is no data
+        if [[ $(cat "$projDir/$DBName/$tableName" | wc -l) == 0 ]]; then
+            echo -e "\n${cyan}No data found${reset}\n"
+        else
+            # get table columns name
+            colNum=$(head -n 1 "$projDir/$DBName/.$tableName" | cut -d',' -f2)
+            colNames=$(tail -n+2 "$projDir/$DBName/.$tableName" | cut -d',' -f1)
 
-           counter=1
-           for col in $colNames
-           do
-               echo "$counter ) select by $col"
-               (( counter++ ))
-           done
+            counter=1
+            for col in $colNames; do
+                echo "$counter ) select by $col"
+                ((counter++))
+            done
 
-           while true; do
+            while true; do
                 read selectedCol
-               if [[ $selectedCol == "" || $selectedCol -lt 1 || $selectedCol -gt $colNum ]]; then
+                if [[ $selectedCol == "" || $selectedCol -lt 1 || $selectedCol -gt $colNum ]]; then
                     echo -e "${yellow}Choose correct value${reset}"
-               else
+                else
                     # selected col name
-                    needle=`echo $colNames | cut -d' ' -f $selectedCol`
-                    # returned values
-                    values=`awk -F ',' -v arg="$selectedCol" '{ print $arg }' "$projDir/$DBName/$tableName"`
+                    needle=$(echo $colNames | cut -d' ' -f $selectedCol)
 
-                    echo -e "${cyan}------ $needle -------"
-                    for val in $values
-                    do
-                        echo $val
-                        echo -e "-------------------"
+                    while true; do
+                        read -p "Enter $needle: " search
+                        if [[ $search == "" ]]; then
+                            echo -e "${yellow}Not valid!${reset}"
+                        else
+                            iter=1
+                            echo -e "\n${cyan}----------------------"
+                            for i in $colNames; do
+                                echo -ne "$i";
+                                if [ $iter -lt $colNum ]; then
+                                    echo -ne "\t| \t"
+                                fi
+                                (( iter++ ))
+                            done
+                            echo -e "\n----------------------"
+
+                            awk -F',' -v arg="$selectedCol" -v ser="$search" 'BEGIN{OFS="\t|\t"} { $1=$1; if ( $arg == ser  ) { print } }' "$projDir/$DBName/$tableName"
+
+                            echo -e "----------------------${reset}\n"
+                            break
+                        fi
                     done
-                    echo -e "${reset}"
-
-                   break
-               fi
-           done
-       fi
+                    break
+                fi
+            done
+        fi
     fi
 
     tablesMenu $DBName
